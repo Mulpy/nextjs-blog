6ca6afb10a3a615c264311867d0cc2a042fe6a86
Gary Topping <toppinggary@hotmail.com>
2023-10-06 13:11:10 +0900

    minor changes to test vercel preview function

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm Gary. I am a junior back end developer living in Japan. I am excited to be learning Next.js through the tutorial on nextjs.org</p>
+        <p>Hi, I'm Gary T. I am a junior back end developer living in Japan. I am excited to be learning Next.js through the tutorial on nextjs.org</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
